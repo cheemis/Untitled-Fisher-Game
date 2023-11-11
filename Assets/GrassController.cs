7920d7c9120a82a43679b50df4996389662f48f2
Sam Feng <fengyuan20000303@gmail.com>
2023-11-11 15:28:07 -0800

    Update GrassController.cs

--- Assets/GrassController.cs
+++ Assets/GrassController.cs
@@ -6,7 +6,7 @@ public class GrassController : MonoBehaviour
 {
     // Start is called before the first frame update
     private float lifeTime = 10f;
-    
+
     void Start()
     {
         
@@ -15,6 +15,20 @@ public class GrassController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        //  decrease the life time
+        lifeTime -= Time.deltaTime;
+
+        // if the life time is less than 0
+        if (lifeTime < 0)
+        {
+            // check if object is off screen
+            Vector3 vpPos = Camera.main.WorldToViewportPoint(transform.position);
+            bool isVisible = vpPos.x >= 0f && vpPos.x <= 1f && vpPos.y >= 0f && vpPos.y <= 1f && vpPos.z > 0f;
+            if (!isVisible)
+            {
+                // destroy the object
+                Destroy(gameObject);
+            }
+        }
     }
 }
