c722d3bd97c91a2b2a8b4f8e266bc4e49dec519e
Sam Feng <fengyuan20000303@gmail.com>
2023-11-11 14:46:44 -0800

    Update FishCollectable.cs

--- Assets/FishCollectable.cs
+++ Assets/FishCollectable.cs
@@ -19,6 +19,7 @@ public class FishCollectable : Collectable
         audioSource.pitch = Random.Range(0.8f, 1.2f);
         audioSource.volume = Random.Range(0.8f, 1.2f);
 
+        // play the sound when fish spawns
         audioSource.Play();
     }
 
