ac69394a3b902c1ad842ea09b728df20075fafe7
cheemis <cheemisdafrog@gmail.com>
2023-11-10 17:18:43 -0800

    boat can be controlled by the player

--- Assets/Our Assets/Scripts/Player/PlayerBoat.cs
+++ Assets/Our Assets/Scripts/Player/PlayerBoat.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -12,9 +13,26 @@ public class PlayerBoat : MonoBehaviour
     private float acceleration = 50f;
     [SerializeField]
     private float maxSpeed = 1000f;
+    //[SerializeField]
+    //private float dragSpeed;
+
+    [Space(30)]
+
+    //rotation variables
     [SerializeField]
     private float rotationSpeed = 50f;
+    [SerializeField]
+    private float maxRotationSpeed = 10f;
+    private float currentRotationSpeed = 0;
 
+    [Space(30)]
+
+    //control variables
+    [SerializeField]
+    private float slowDownSpeed = 1;
+    private bool canControlBoat = true;
+
+    [Space(30)]
 
     //component variables
     private Rigidbody rb;
@@ -36,26 +54,60 @@ public class PlayerBoat : MonoBehaviour
     private void ReadInput()
     {
         //rotate boat left/right
-        //RotatePlayer(Input.GetAxis("Horizontal"));
+        RotatePlayer(Input.GetAxis("Horizontal"));
+
 
         //move boat forward/backwards input
         MovePlayer(Input.GetAxis("Vertical"));
+
+        //Debug.Log("current velocity: " + rb.velocity.magnitude);
     }
     
     private void MovePlayer(float direction)
     {
-        if(rb.velocity.magnitude < maxSpeed)
-        {
-            rb.AddForce(transform.forward * direction * acceleration * Time.deltaTime, ForceMode.Acceleration);
-        }
+        float x = 0;
+        float y = 0;
+        float z = 0;
+        Vector3 newVelocity = Vector3.zero;
+
+        //apply the direction held
+        rb.AddForce(transform.forward * direction * acceleration * Time.deltaTime, ForceMode.Force);
+        newVelocity = rb.velocity;
+
+        //clamp the direction so it doesn't go too fast
+        x = Mathf.Clamp(newVelocity.x, -maxSpeed, maxSpeed);
+        y = Mathf.Clamp(newVelocity.y, -maxSpeed, maxSpeed);
+        z = Mathf.Clamp(newVelocity.z, -maxSpeed, maxSpeed);
+
+        //apply new velocity
+        rb.velocity = new Vector3(x,y,z);
     }
 
-    /*
+
     private void RotatePlayer(float rotationDirection)
     {
-        rb.MoveRotation()
+        Quaternion currentRotation = rb.rotation;
+
+        //gradually increase turning speed as you speed up ==== MAGIC NUMBERS ====
+        float currentSpeed = Mathf.Clamp(rb.velocity.magnitude / maxSpeed, .25f, 1);
+        currentRotationSpeed += rotationSpeed * rotationDirection * currentSpeed;
+        currentRotationSpeed = Mathf.Clamp(currentRotationSpeed, -maxRotationSpeed, maxRotationSpeed) * Time.deltaTime;
+
+        rb.MoveRotation(currentRotation * Quaternion.Euler(0, currentRotationSpeed, 0));
     }
-    */
+
+    IEnumerator SlowDownBoat()
+    {
+        canControlBoat = false;
+        while(rb.velocity.magnitude > .1)
+        {
+            rb.velocity -= slowDownSpeed;
+            yield return null;
+        }
+        yield return null;
+
+
+    }
 
 
 }
