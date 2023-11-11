8b7a05ef96af89731b1a9a9fc12127b943f7f94c
Sam Feng <fengyuan20000303@gmail.com>
2023-11-11 14:45:40 -0800

    Added Fish Noises

--- Assets/FishCollectable.cs
+++ Assets/FishCollectable.cs
@@ -4,10 +4,22 @@ using UnityEngine;
 
 public class FishCollectable : Collectable
 {
+    public AudioClip[] sounds;
+    private AudioSource audioSource;
+
     // Start is called before the first frame update
     void Start()
     {
+        audioSource = GetComponent<AudioSource>();
+
+        // play a random sound when spawned
+        audioSource.clip = sounds[Random.Range(0, sounds.Length)];
         
+        // randomize pitch and volume
+        audioSource.pitch = Random.Range(0.8f, 1.2f);
+        audioSource.volume = Random.Range(0.8f, 1.2f);
+
+        audioSource.Play();
     }
 
     // Update is called once per frame
